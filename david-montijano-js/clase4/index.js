239b23c763d59ed7b4ee4e3a15db28d433a0757d
David Montijano <davidmontijano2@gmail.com>
2022-09-13 21:39:51 -0300

    funciones

---
+++ david-montijano-js/clase4/index.js
@@ -0,0 +1,64 @@
+
+
+/* function saludar(){
+    let nombre = prompt("Ingrese su nombre");
+    let mensaje = `Hola ${nombre} como estas?`;
+
+    alert(mensaje);
+}
+
+saludar(); */
+
+
+/* function saludar (nombre, edad){
+    let mensaje = `Hola ${nombre} tu edad es ${edad} `;
+    alert(mensaje)
+}
+
+let nombre = prompt("ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+
+saludar (nombre, edad); */
+
+
+
+/* let total = 0;
+let precioProducto = 0;
+
+function costoCompra();
+
+switch(precioFinal){
+
+    case 2500 :
+    
+        break;
+
+    case "2":
+        
+        break;
+
+    case "3":
+        
+        break;
+
+    case "4":
+        
+        break;
+
+    default:
+        alert("Opcion no encontrada")
+        break;
+    }
+ */
+
+
+
+    function adicion(precio, porcentaje){
+        return precio + ((precio * porcentaje)/100)
+    }
+    for (let index = 0; index < 10; index++) {
+        let resultado = adicion(parseFloat(prompt("INGRESAR PRECIO"))
+        ,parseFloat(prompt("INGRESAR %")));
+        alert(resultado);
+}
+
